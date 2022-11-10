938f9ff3f060c24c891a69ef249fceec8419cfcc
LucasOliveiraSantana <lucasoliveirasantana.dev@gmail.com>
2022-11-10 11:38:06 -0300

    3 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,60 @@
 import { Header } from './components/Header'
-import {Sidebar} from "./components/Sidebar"
-import {Post} from "./components/Post"
+import { Sidebar } from "./components/Sidebar"
+import { Post } from "./components/Post"
 
 import './global.css'
 import styles from './App.module.css'
 
-
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'http://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+        {type:"paragraph", content:"Fala galeraa 👋"},
+        {type:"paragraph", content:"Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀"},
+        {type:"link", content:"jane.design/doctorcare"},
+    ],
+    publishedAt: new Date("2022-11-05 20:00:00")
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/AdrianoBinhara.png',
+      name: 'Binhara',
+      role: 'Developer Xamarin'
+    },
+    content: [
+        {type:"paragraph", content:"Fala galeraa 👋"},
+        {type:"paragraph", content:"Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀"},
+        {type:"link", content:"jane.design/doctorcare"},
+    ],
+    publishedAt: new Date("2022-11-02 20:00:00")
+  },
+]
 
 function App() {
   return (
-  <div>
-    <Header />
-    <div className={styles.wrapper}>
-      <Sidebar />
-      <div>
-        <Post />
-        <Post />
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <div>
+          {posts.map(post=>{
+            return (
+              <Post
+                author={post.author} 
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </div>
       </div>
     </div>
-  </div>
 
   )
 }
